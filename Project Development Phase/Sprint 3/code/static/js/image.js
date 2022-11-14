42cbcb09d447f98b6429f528545380ebfa572d36
AKASH V S <106974610+AKASH7707@users.noreply.github.com>
2022-11-14 22:07:51 +0530

    Delete image.js

--- Project Development Phase/Sprint 3/code/static/js/image.js
+++
@@ -1,6 +0,0 @@
-var loadFile = function(event) {
-    document.getElementById('submit').disabled = false;
-    document.getElementById('submit').style.backgroundColor='#ff0000';
-    var image = document.getElementById('output');
-    image.src = URL.createObjectURL(event.target.files[0]);
-};
